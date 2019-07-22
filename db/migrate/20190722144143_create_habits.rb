453054c8737f6a4cf05014c79f87b889c1f86a79
dommarr <dom.marrone@gmail.com>
2019-07-22 10:45:02 -0400

    Create new resource
    Created rails resource for habits table.

---
+++ db/migrate/20190722144143_create_habits.rb
@@ -0,0 +1,10 @@
+class CreateHabits < ActiveRecord::Migration[5.2]
+  def change
+    create_table :habits do |t|
+      t.string :habit_title
+      t.integer :streak
+
+      t.timestamps
+    end
+  end
+end
