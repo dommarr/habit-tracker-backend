1b58e7e404def1367a52fa683f4e4b26d2512f31
dommarr <dom.marrone@gmail.com>
2019-08-17 23:09:31 -0400

    Update goal complete column default
    Updated goal complete column default to false.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_17_210239) do
+ActiveRecord::Schema.define(version: 2019_08_18_030523) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,7 +26,7 @@ ActiveRecord::Schema.define(version: 2019_08_17_210239) do
   create_table "goals", force: :cascade do |t|
     t.string "description"
     t.date "due_date"
-    t.boolean "complete"
+    t.boolean "complete", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id"
